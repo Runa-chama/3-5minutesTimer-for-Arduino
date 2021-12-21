5e4bef5ae7f052f03fd41427e2253ece65a849af
Runa-chama <88036533+Runa-chama@users.noreply.github.com>
2021-12-21 10:35:22 +0900

    Update Timer.ino

--- Timer.ino
+++ Timer.ino
@@ -7,8 +7,10 @@
 #define beep_pin A0
 
 const int led [5] = {5, 6, 7, 8, 9};
+
 boolean count = false;
 boolean finish = false;
+
 int count_time = 180;
 int led_on = HIGH;
 int led_num = 0;
@@ -19,14 +21,15 @@ int beep_tone = 1500;
 
 void counter() {
   if (time_count != (count_time * 2) && count == true) {
+    
     for (int n = 0; n < 5; n++)
       digitalWrite(led[n], HIGH);
+    
     led_on = !led_on;
     minute = time_count % 120 == 0 ? --minute : minute;
+    
     for (int n = 0; n < minute; ++n)
-    {
       digitalWrite(led[n], led_on);
-    }
   }
   else
   {
@@ -40,6 +43,7 @@ void counter() {
   {
     for (int a = 0; a < 5; a++)
       digitalWrite(led[a], HIGH);
+    
     tone(beep_pin, beep_tone, 400);
     finish = led_num > 4 ? false : true;
     led_num = led_num > 4 ? 0 : led_num;
@@ -61,22 +65,22 @@ void setup() {
   pinMode(time_sw, INPUT_PULLUP);
   pinMode(start_sw, INPUT_PULLUP);
   pinMode(beep_pin, OUTPUT);
-  for (int n = 0; n < 5; n++) {
+  
+  for (int n = 0; n < 5; n++)
     pinMode(led[n], OUTPUT);
-  }
-  for (int n = 0; n < 3; n++) {
+    
+  for (int n = 0; n < 3; n++)
     digitalWrite(led[n], LOW);
-  }
-  for (int n = 3; n < 5; n++) {
+    
+  for (int n = 3; n < 5; n++)
     digitalWrite(led[n], HIGH);
-  }
+    
   tone(beep_pin, beep_tone, 50);
   delay(50);
   tone(beep_pin, beep_tone, 50);
+  
   FlexiTimer2::set(250, counter);
   FlexiTimer2::start();
-  digitalWrite(13, LOW);
-
 }
 
 void loop() {
@@ -84,15 +88,18 @@ void loop() {
   {
     for (int n = 0; n < 5; n++)
       digitalWrite(led[n], HIGH);
+    
     count_time = count_time == 180 ? 300 : 180;
     minute = count_time / 60;
     cache_minute = minute;
+    
     for (int n = 0; n < minute; n++)
     {
       digitalWrite(led[n], LOW);
       tone(beep_pin, beep_tone, 50);
       delay(150);
     }
+    
     count = false;
     delay(300);
   }
@@ -116,12 +123,13 @@ void loop() {
       time_count = 0;
       count_time = cache_minute * 60;
       minute = cache_minute;
+      
       for (int n = 0; n < 5; n++)
         digitalWrite(led[n], HIGH);
+      
       for (int n = 0; n < cache_minute; n++)
-      {
         digitalWrite(led[n], LOW);
-      }
+      
       delay(1000);
     }
   }
